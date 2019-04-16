49524d0af9b4db7feb2aa984f8d99f593477f95e
Esperance <48394404+World-Cup@users.noreply.github.com>
2019-04-16 17:07:03 -0400

    Git review

--- module-4/week-6/day-1/index.js
+++ module-4/week-6/day-1/index.js
@@ -2,3 +2,6 @@
 
 const numberOfStates = 50;
 console.log(numberOfStates);
+//git repo, commit, branch, merge, fork, pull request
+//basics of terminal 1.5 the coding train. *command not found: git not installed properly/mispelling
+//Git repository-clone to a computer. Git
