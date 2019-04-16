18549c85a980c6ad70302be813b4186ea950b8d4
World-Cup <48394404+World-Cup@users.noreply.github.com>
2019-04-16 17:15:12 -0400

    Add files via upload

---
+++ module-4/index.js
@@ -0,0 +1,7 @@
+// Change this file to do something else!
+
+const numberOfStates = 50;
+console.log(numberOfStates);
+//git repo, commit, branch, merge, fork, pull request
+//basics of terminal 1.5 the coding train. *command not found: git not installed properly/mispelling
+//Git repository-clone to a computer. Git
